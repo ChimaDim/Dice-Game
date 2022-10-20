5ffc962866e4e968d843e39ad092492da77b4c95
ChimaDim <112209283+ChimaDim@users.noreply.github.com>
2022-10-20 15:30:51 -0700

    Update index.js
    Little touches, here and there...

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var randomImageSource = "images/" + randomDiceImage;
+// var randomImageSource = "images/" + randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", randomImageSource);
+image1.setAttribute("src", randomDiceImage);
 
 
                   // Second Dice
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
 
 var image2 = document.querySelectorAll("img")[1];
 
